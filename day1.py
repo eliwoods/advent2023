b68c2ad8cf3185182f26f09fd60c19e3109fb599
Eli Woods <eli@recentiveanalytics.com>
2023-12-04 00:25:43 -0800

    more cleanup

--- day1.py
+++ day1.py
@@ -20,41 +20,42 @@ def get_data(file: str) -> list[str]:
         return f.read().splitlines()
 
 
-def _process_line_regex(line: str) -> int:
+def process_line_regex(line: str) -> int:
     matches = INT_MATCH.findall(line)
     nums = list(map(lambda x: NUMS[x] if x in NUMS else x, matches))
-    ans = int(f'{nums[0]}{nums[-1]}')
-    return ans
+    return int(f'{nums[0]}{nums[-1]}')
 
 
 def solve_part_1(file: str) -> int:
     data = get_data(file)
-    vals = list(map(_process_line_regex, data))
+    vals = map(process_line_regex, data)
     return sum(vals)
 
 
-def _process_line_literals(line: str) -> int:
+def process_line_literals(line: str) -> int:
     nums = []
     for i in range(len(line)):
         for literal, num in NUMS.items():
             if ''.join(line[i:i + len(literal)]) == literal or line[i] == str(num):
                 nums.append(num)
-    ans = int(f'{nums[0]}{nums[-1]}')
-    return ans
+                break
+    return int(f'{nums[0]}{nums[-1]}')
 
 
 def solve_part_2(file: str) -> int:
     data = get_data(file)
-    vals = list(map(_process_line_literals, data))
+    vals = map(process_line_literals, data)
     ans = sum(vals)
     return ans
 
 
 def main():
     assert solve_part_1('day1-part1-test.txt') == 142
+    # 55712
     print('Part 1:', solve_part_1('day1.txt'))
 
     assert solve_part_2('day1-part2-test.txt') == 281
+    # 55413
     print('Part 2:', solve_part_2('day1.txt'))
 
 
