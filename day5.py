517b7174356f17cc7a66946e2071d6693ac29dfd
Eli Woods <eli@recentiveanalytics.com>
2023-12-08 01:56:11 -0800

    too tired

--- day5.py
+++ day5.py
@@ -54,6 +54,25 @@ def apply_mapping(seeds: list[int], map_rows: list[MapRow]) -> list[int]:
     return output
 
 
+def merge_ranges(ranges: list[list[int]]) -> list[list[int]]:
+    if len(ranges) < 2:
+        return ranges
+
+    sorted_ranges = sorted(ranges, key=lambda x: x[0])
+
+    merged_ranges = []
+    prev_interval = sorted_ranges[0]
+    for interval in sorted_ranges[1:]:
+        if prev_interval[1] >= interval[0]:
+            prev_interval[1] = max(interval[1], prev_interval[1])
+        else:
+            merged_ranges.append(prev_interval)
+            prev_interval = interval
+    merged_ranges.append(prev_interval)
+
+    return merged_ranges
+
+
 # TODO(ew) need to handle partial ranges for mapping
 def apply_range_mapping(seed_ranges: list[list[int]], map_rows: list[MapRow]) -> list[list[int]]:
     output = []
@@ -61,31 +80,45 @@ def apply_range_mapping(seed_ranges: list[list[int]], map_rows: list[MapRow]) ->
     for seed_range in seed_ranges:
         seed_range_min = seed_range[0]
         seed_range_max = seed_range[1]
-        full_range_mapped = False
-        lower_range_mapped = False
-        upper_range_mapped = False
+        mapped = False
 
+        range_output = []
+        # TODO Need to consider the fact that overlaps could split range into >2 parts; don't think we have to merge tho
         for map_row in map_rows:
-            # Entire seed range is mapped
             if map_row['source'] <= seed_range_min <= map_row['source'] + map_row['range'] and map_row['source'] <= seed_range_max <= map_row['source'] + map_row['range']:
                 print(f'Seed range {seed_range=} maps to row {map_row=}')
-                output.append([seed_range_min + map_row['diff'], seed_range_max + map_row['diff']])
-                full_range_mapped = True
+                mapped_range = [seed_range_min + map_row['diff'], seed_range_max + map_row['diff']]
+                range_output.append(mapped_range)
+                mapped = True
                 break
             if map_row['source'] <= seed_range_min <= map_row['source'] + map_row['range'] and not(map_row['source'] <= seed_range_max <= map_row['source'] + map_row['range']):
-                partial_range = [seed_range_min + map_row['diff'], map_row['source'] + map_row['range'] + map_row['diff']]
-                print(f'Seed range {seed_range=} partially maps to row {map_row=}, upper bound out of map range. Partial range {partial_range=}')
-                # lower_range_mapped = True
-                # break
+                mapped_range = [seed_range_min + map_row['diff'], map_row['source'] + map_row['range'] + map_row['diff'] - 1]
+                unmapped_range = [map_row['source'] + map_row['range'], seed_range_max]
+                range_output.extend([mapped_range, unmapped_range])
+                print(f'Seed range {seed_range=} partially maps to row {map_row=}, upper bound out of map range.')
+                print(f'Mapped range {mapped_range=}')
+                print(f'Unmapped range {unmapped_range=}')
+                mapped = True
+                continue
             if not(map_row['source'] <= seed_range_min <= map_row['source'] + map_row['range']) and map_row['source'] <= seed_range_max <= map_row['source'] + map_row['range']:
-                partial_range = [map_row['source'] + map_row['diff'], seed_range_max]
+                mapped_range = [map_row['source'] + map_row['diff'], seed_range_max + map_row['diff']]
+                # unmapped_range = [seed_range_min, seed_range_max - map_row['source']]
+                unmapped_range = [seed_range_min, map_row['dest'] - map_row['diff'] - 1]
+                range_output.extend([mapped_range, unmapped_range])
                 print(f'Seed range {seed_range=} partially maps to row {map_row=}, lower bound out of map range.')
-                # upper_range_mapped = True
-                # break
+                print(f'Mapped range {mapped_range=}')
+                print(f'Unmapped range {unmapped_range=}')
+                mapped = True
+                continue
 
-        if not full_range_mapped:
+        if not mapped:
             print(f'Seed range {seed_range=} does not map to any rows')
-            output.append([seed_range_min, seed_range_max])
+            unmapped_range = [seed_range_min, seed_range_max]
+            range_output.append(unmapped_range)
+
+        # TODO(ew) I don't think we have to do this
+        # output += merge_ranges(range_output)
+        output += range_output
 
     return output
 
@@ -101,16 +134,17 @@ def solve_part1(file: str) -> int:
     return min(next_seeds)
 
 
-# TODO(ew) actually solve this. Want to avoid brute forcing, I feel like there is a more clever approach.
 def solve_part2(file: str) -> int:
     maps, seeds = get_data(file)
     seed_ranges = [[seeds[2*i], seeds[2*i] + seeds[2*i+1]] for i in range(len(seeds)//2)]
+
     next_seed_ranges = seed_ranges
     for name, map_rows in maps.items():
         print(f'### Checking mapping for {name} ###')
         next_seed_ranges = apply_range_mapping(next_seed_ranges, map_rows)
         print()
 
+    print(sorted(next_seed_ranges))
     seeds = []
     for seed_range in next_seed_ranges:
         seeds.extend(seed_range)
@@ -123,7 +157,10 @@ def main() -> None:
     # # 107430936
     # print('Part 1:', solve_part1('day5-actual.txt'))
 
+    # supposed expected output without merging
+    # [[46, 56], [56, 60], [60, 61], [82, 85], [86, 90], [94, 97], [97, 99]]
     assert solve_part2('day5-test.txt') == 46
+    # print('Part 2:', solve_part2('day5-actual.txt'))
 
 
 if __name__ == '__main__':
